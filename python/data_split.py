171011d76b6534adb491d9b83ed7663ea7a0435e
Abraham Epton <abraham.epton@gmail.com>
2014-03-17 00:30:52 -0500

    Adding county data

--- python/data_split.py
+++ python/data_split.py
@@ -11,7 +11,7 @@ def load_and_split(path, name_prefix, name_property):
     obj = json.loads(fh.read())
     fh.close()
     for i in obj['features']:
-        name = '%s_%s.geojson' % (name_prefix, i['properties'][name_property])
+        name = '%s_%s.geojson' % (name_prefix, i['properties'][name_property].replace(' ', '_'))
         print 'Starting %s' % name
         newobj = deepcopy(obj)
         newobj['features'] = [deepcopy(i)]
@@ -24,6 +24,8 @@ if __name__ == '__main__':
     #state_rep_property = 'SLDLST'
     #cook_comms = 'cook_county_comm_10.geojson'
     #cook_comm_property = 'District_N'
-    cook_subcircuits = 'cook_subcircuits.geojson'
-    cook_subcircuit_property = 'DISTRICT'
-    load_and_split('%s%s' % (DATA_PATH_PREFIX, cook_subcircuits), 'cook_subcircuits', cook_subcircuit_property)
+    #cook_subcircuits = 'cook_subcircuits.geojson'
+    #cook_subcircuit_property = 'DISTRICT'
+    counties = 'counties_09.geojson'
+    counties_property = 'NAME'
+    load_and_split('%s%s' % (DATA_PATH_PREFIX, counties), 'counties', counties_property)
