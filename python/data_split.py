69aba65cf3f1cc8fb69c65a9abdd7c2b32767ec0
Abraham Epton <abraham.epton@gmail.com>
2014-03-17 00:46:07 -0500

    Adding congressional boundaries

--- python/data_split.py
+++ python/data_split.py
@@ -11,6 +11,8 @@ def load_and_split(path, name_prefix, name_property):
     obj = json.loads(fh.read())
     fh.close()
     for i in obj['features']:
+        #print i['properties']
+        #break
         name = '%s_%s.geojson' % (name_prefix, i['properties'][name_property].replace(' ', '_'))
         print 'Starting %s' % name
         newobj = deepcopy(obj)
@@ -22,12 +24,14 @@ def load_and_split(path, name_prefix, name_property):
 if __name__ == '__main__':
     #state_reps = 'state_reps_09.geojson'
     #state_rep_property = 'SLDLST'
-    state_sens = 'state_sens_09.geojson'
-    state_sen_property = 'SLDUST'
+    #state_sens = 'state_sens_09.geojson'
+    #state_sen_property = 'SLDUST'
     #cook_comms = 'cook_county_comm_10.geojson'
     #cook_comm_property = 'District_N'
     #cook_subcircuits = 'cook_subcircuits.geojson'
     #cook_subcircuit_property = 'DISTRICT'
     #counties = 'counties_09.geojson'
     #counties_property = 'NAME'
-    load_and_split('%s%s' % (DATA_PATH_PREFIX, state_sens), 'state_sens/state_sens', state_sen_property)
+    cong = 'pa_97-14_cong.geojson'
+    cong_property = 'District_N'
+    load_and_split('%s%s' % (DATA_PATH_PREFIX, cong), 'congress/cong', cong_property)
